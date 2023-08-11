14734a5143aa67dee59c59ab2f806ffc94cb21c8
dsdb <dbsyncloud@gmail.com>
2023-08-11 10:51:32 +0530

    Commit:v1

--- app.py
+++ app.py
@@ -20,33 +20,34 @@ def main():
     selected_category = st.selectbox("Select a Category", categories)
     st.write("Selected Category:", selected_category)
    
-   # Process the news content
-    visualization_btn = st.button("Generate visualization")
-    if visualization_btn:
-        media_represntation.process_news_content()          
-        st.write("Processed data for visualization")
+   # Process the news content    
+    if st.button("Generate visualization"):
+        generate_visualtion(selected_category)     
 
-    # Process the data and generate sentiment chart    
-    if selected_category in media_represntation.proccessed_df:
-        sentiment_result = media_represntation.proccessed_df[selected_category]           
-        sentiment_chart = plt.figure()
-        sns.barplot(data=sentiment_result, x="sentiment", y="sentiment_count", palette='Set2')
-        plt.title(f"Sentiment Analysis for {selected_category} Category")
-        plt.xlabel("Sentiment")
-        plt.ylabel("Count")
-        st.pyplot(sentiment_chart)
-        
-        # Process the data and generate keyword chart
-        keyword_result = media_represntation.proccessed_df[selected_category]                     
-        keyword_chart = plt.figure()
-        sns.barplot(data=keyword_result, x="keyword_present", y="keyword_count", palette='Set1')
-        plt.title(f"Keyword Representation for {selected_category} Category")
-        plt.xlabel("Keyword")
-        plt.ylabel("Count")
-        st.pyplot(keyword_chart)
-
-    else:
-        st.write(f"No data available for the selected category: {selected_category}")
+    def generate_visualtion(category):        
+        # Load data and process based on selected category
+        media_represntation.process_news_content()      
+        # Process the data and generate sentiment chart    
+        if category in media_represntation.proccessed_df:
+            sentiment_result = media_represntation.proccessed_df[category]           
+            sentiment_chart = plt.figure()
+            sns.barplot(data=sentiment_result, x="sentiment", y="sentiment_count", palette='Set2')
+            plt.title(f"Sentiment Analysis for {category} Category")
+            plt.xlabel("Sentiment")
+            plt.ylabel("Count")
+            st.pyplot(sentiment_chart)
+            
+            # Process the data and generate keyword chart
+            keyword_result = media_represntation.proccessed_df[category]                     
+            keyword_chart = plt.figure()
+            sns.barplot(data=keyword_result, x="keyword_present", y="keyword_count", palette='Set1')
+            plt.title(f"Keyword Representation for {category} Category")
+            plt.xlabel("Keyword")
+            plt.ylabel("Count")
+            st.pyplot(keyword_chart)
+            st.write("Processed data for visualization")
+        else:
+            st.write(f"No data available for the selected category: {category}")
 
 
 if __name__ == "__main__":
